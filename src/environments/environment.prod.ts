c56204763dfe5b16214406f361dac86f50de81b4
Kevin Tchubinsky <kevinchu19@gmail.com>
2022-04-27 23:00:12 -0300

    compilado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@
 export const environment = {
   production: true,
   base_url: 'https://pedidos-ws.anclaflex.com.ar/api/',
-  pdf_url: 'http://191.102.18.36:22771'
+  pdf_url: 'http://191.102.18.36:22771/api/'
 };
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -3,5 +3,5 @@ export const environment = {
   staging: true,
   production: false,
   base_url: 'http://165.22.37.224/api/',
-  pdf_url: 'http://191.102.18.36:22772'
+  pdf_url: 'http://191.102.18.36:22772/api/'
 };
